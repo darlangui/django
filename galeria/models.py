fc2b7033cfa5e2c9aaf16385428a4207bdc625bc
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-18 16:57:45 -0300

    admin - add categoria in bd

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Foto(models.Model):
+
+    opcao_categoria = [
+        ("NUBULOSA", "nubulosa"),
+        ("ESTRELA", "estrela"),
+        ("GALAXIA", "galaxia"),
+        ("PLANETA", "planeta"),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=200, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=opcao_categoria, default="")
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
