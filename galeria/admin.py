de175d0a48f6511365d7e613b08451540da19013
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-18 16:51:58 -0300

    admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galeria.models import Foto
 
-# Register your models here.
+class listandoFotos(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+
+admin.site.register(Foto, listandoFotos)
