93ac8126f809084769a2b89f77496bc1e3fca083
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-19 19:28:29 -0300

    search add

--- galeria/views.py
+++ galeria/views.py
@@ -10,3 +10,12 @@ def index(request):
 def imagem(request, foto_id):
     foto = get_object_or_404(Foto, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"foto": foto})
+
+
+def buscar(request):
+    fotografias = Foto.objects.order_by("data").filter(publicada=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
