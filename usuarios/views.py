2f6881e8937b34e8d509495e0fa0f60e86d77605
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-21 08:46:25 -0300

    form login and cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from usuarios.form import LoginForms, CadastroForm
 
 def login(request):
-    return render(request, "usuario/login.html")
+    form = LoginForms()
+    return render(request, "usuario/login.html", {"form": form})
 
 
 def cadastro(request):
-    return render(request, "usuario/cadastro.html")
+    form = CadastroForm()
+    return render(request, "usuario/cadastro.html", {"form": form})
+
+
+
